0348b7614cb9bb8a8ea9654180ddc63568f5d6a5
mona ali <monaali.5778@gmail.com>
2020-04-24 15:39:42 +0200

    featching feature component data

--- src/redux/Reducer/FeatureReducer.js
+++ src/redux/Reducer/FeatureReducer.js
@@ -2,10 +2,10 @@ import * as type from "../Action/type";
 
 const initialState = {
   features:[
-    // {id:1, icon:'globe', title:'Explore the world', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
-    // {id:2, icon:'heart', title:'Explore the world', description:'Lorem ipsum dolor consectetur. Aperiam, sapiente aspernatur.'},
-    // {id:3, icon:'compass', title:'Live a healthier life', description:'Lorem ipsum sit amet consectetur adipisicing elit.'},
-    // {id:4, icon:'map', title:'Meet nature', description:'Lorem ipsum dolor consectetur adipisicing elit. Aperiam, sapiente aspernatur'},
+    {id:1, icon:'globe', title:'Explore the world', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {id:2, icon:'heart', title:'Explore the world', description:'Lorem ipsum dolor consectetur. Aperiam, sapiente aspernatur.'},
+    {id:3, icon:'compass', title:'Live a healthier life', description:'Lorem ipsum sit amet consectetur adipisicing elit.'},
+    {id:4, icon:'map', title:'Meet nature', description:'Lorem ipsum dolor consectetur adipisicing elit. Aperiam, sapiente aspernatur'},
   ],
   error: null
 }
